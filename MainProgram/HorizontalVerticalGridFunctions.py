6a396c2bef90a9e0ef692bcaa5802f9e7abbb71a
JokNavi <JokNavi1@gmail.com>
2022-09-08 13:24:37 +0200

    Fixed comments

--- MainProgram/HorizontalVerticalGridFunctions.py
+++ MainProgram/HorizontalVerticalGridFunctions.py
@@ -9,12 +9,12 @@ class DefaultLayout:
         plt.xlabel("X axis")
         plt.ylabel("Y axis")
         plt.grid()
-    # Creates the vectors in our program
 
+    # Creates the vectors in our program
     def VectorEditor(Height, Width, Vectors, Space):
         VectorAmount = Vectors + 1
-        # Creates basis vectors acording to height and width (XInput,YInput)
 
+        # Creates basis vectors acording to height and width (XInput,YInput)
         def BasisVectors(XInput, YInput):
             def BasisX():
                 x = np.array([0, 0])
@@ -27,20 +27,20 @@ class DefaultLayout:
                 plt.plot(x, y, color='r')
             BasisX()
             BasisY()
-        # Creates all the X vectors based on the Basis vector
 
+        # Creates all the X vectors based on the Basis vector
         def CreateVectorsX(Spacing, FinalHeight):
             x = np.array([Spacing, Spacing])
             y = np.array([FinalHeight, 0])
             plt.plot(x, y, color='b')
-        # Creates all the Y vectors based on the Basis vector
 
+        # Creates all the Y vectors based on the Basis vector
         def CreateVectorsY(Spacing, FinalWidth):
             x = np.array([0, FinalWidth])
             y = np.array([Spacing, Spacing])
             plt.plot(x, y, color='b')
-        # Modifies the input values and calls the vector creating functions
 
+        # Modifies the input values and calls the vector creating functions
         def SetVectors():
             Spacing = 0
             for Vector in range(VectorAmount):
