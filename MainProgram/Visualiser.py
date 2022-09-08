1184ab12c31afa992e27a31bac557adfeef3518b
JokNavi <JokNavi1@gmail.com>
2022-09-08 13:22:33 +0200

    Fixed layout

--- MainProgram/Visualiser.py
+++ MainProgram/Visualiser.py
@@ -1,11 +1,24 @@
 import matplotlib.pyplot as plt
-from DefaultGridFunctions import DefaultLayout
+from MainProgram.LinearVectorTransformations import DefaultLayout
 DL = DefaultLayout
+
+
 class RunProgram:
-  #Change these inputs (Vectors and Space)
-  VECTORS = 10
-  SPACE = 1
-  DL.MyGrid()
-  DL.VectorEditor(1, 1, VECTORS, SPACE)
-  plt.show()
+    def SquareGrid():
+        # Change these inputs (Vectors and Space)
+        VECTORS = 10
+        SPACE = 1
+        DL.MyGrid()
+        DL.VectorEditor(1, 1, VECTORS, SPACE)
+        plt.show()
+
+    def VectorTransformations():
+        # Change these inputs (Vectors and Space)
+        VECTORS = 10
+        SPACE = 1
+        DL.MyGrid()
+        DL.VectorEditor(1, 1, VECTORS, SPACE)
+        plt.show()
+
+
 RunProgram()
