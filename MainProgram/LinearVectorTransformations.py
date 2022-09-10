01bf5fdfe7e0d957acb7541309ade6526814d749
JokNavi <JokNavi1@gmail.com>
2022-09-10 12:40:16 +0200

    bump

--- MainProgram/LinearVectorTransformations.py
+++ MainProgram/LinearVectorTransformations.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 
 class DefaultLayout:
 
-    def __init__(self, Height, Width, Vectors):
+    def __init__(self, Height, Width, Vectors, Spacing):
         self.XInput = Width
         self.YInput = Height
         self.VectorAmount = Vectors - 1
+        self.Spacing = Spacing
 
     def MyGrid(self):
         plt.title("Linear Vector Transformations")
@@ -18,12 +19,12 @@ class DefaultLayout:
         # Creates basis vectors acording to height and width (XInput,YInput)
     def BasisVectors(self):
 
-        def BasisX():
+        def BasisY():
             x = np.array([0, 0])
             y = np.array([self.XInput, 0])
             plt.plot(x, y, color='r')
 
-        def BasisY():
+        def BasisX():
             x = np.array([0, self.YInput])
             y = np.array([0, 0])
             plt.plot(x, y, color='r')
@@ -36,12 +37,12 @@ class DefaultLayout:
         EditedY = self.YInput
 
         def TestVector(XCoord, YCoord):
-            x = np.array([XCoord, 0])
-            y = np.array([0, YCoord])
+            y = np.array([XCoord, 0])
+            x = np.array([0, YCoord])
             plt.plot(x, y, color='b')
 
         TestVector(EditedX, EditedY)
         for vector in range(self.VectorAmount):
-            EditedX = EditedX + 1
-            EditedY = EditedY + 1
+            EditedY = EditedY + self.Spacing
+            EditedX = EditedX + self.Spacing
             TestVector(EditedX, EditedY)
