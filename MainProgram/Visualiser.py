cf9afc7218b1c82b3db52042c88e3af2f16084b2
JokNavi <JokNavi1@gmail.com>
2022-09-11 15:56:57 +0200

    test

--- MainProgram/Visualiser.py
+++ MainProgram/Visualiser.py
@@ -1,14 +1,6 @@
 import matplotlib.pyplot as plt
 from LinearVectorTransformations import DefaultLayout
 from HorizontalVerticalGridFunctions import DefaultLayoutS
-<<<<<<< HEAD
-DLS = DefaultLayoutS
-DL = DefaultLayout
-=======
-
-
->>>>>>> 139da34931291fac260e091722e85d8b63d4ee49
-
 
 
 class RunProgram:
@@ -16,10 +8,7 @@ class RunProgram:
         # Change these inputs (Vectors and Space)
         VECTORS = 10
         SPACE = 1
-<<<<<<< HEAD
-=======
         DLS = DefaultLayoutS
->>>>>>> 139da34931291fac260e091722e85d8b63d4ee49
         DLS.MyGrid()
         DLS.VectorEditor(1, 1, VECTORS, SPACE)
         plt.show()
@@ -36,9 +25,6 @@ class RunProgram:
         DL.VectorsGroupOne()
         DL.VectorGroupTwo()
         plt.show()
-    VectorTransformations()
-    #SquareGrid()
-
     VectorTransformations()
     #SquareGrid()
 
